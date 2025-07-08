3d204bf87b7f2b307f30db02ffabdd7a3dc2ed55
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-07-08 08:43:33 +0300

    feat: restore db connection setup and implement offers and shipments service functions

---
+++ lib/offers/offers.ts
@@ -0,0 +1,8 @@
+import { Offers } from "../validation";
+
+export async function getAllOffers(){}
+export async function getOfferById(id: string) {}
+export async function createOffer(data: Offers) {}
+export async function updateOffer(id: string, data: Offers) {}
+export async function deleteOffer(id: string) {}
+export async function getOffersByUserId(userId: string) {}
---
+++ lib/shipments/shipments.ts
@@ -0,0 +1,8 @@
+import { Shipment } from "../validation";
+
+export async function getAllShipments(){}
+export async function getShipmentById(id: string) {}
+export async function createShipment(data: Shipment) {}
+export async function updateShipment(id: string, data: Shipment) {}
+export async function deleteShipment(id: string) {}
+export async function getShipmentsByUserId(userId: string) {}
