fc554c5c4befa79da73f81673b9d6094225450ea
Kiril Zyapkov <appdeveloperjs47@gmail.com>
2025-10-11 21:03:41 +0300

    remove unused ReactElement.

--- components/charts/BarChartShipments.tsx
+++ components/charts/BarChartShipments.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { Card } from '../ui/card';
 
